56b186f8c93add96142d359092db49d923a98145
clarammb <claramendiluce@gmail.com>
2024-12-11 14:33:52 +0100

    Creación de SlideShow y Archivo de JavaScript
    Lo hemos hecho juntas en Discord desde mi cuenta :). Tenemos que revisar porqué no se ven las fotos

---
+++ script.js
@@ -0,0 +1,33 @@
+// JavaScript for Slideshow
+let slideIndex = 0;
+showSlides(slideIndex);
+
+// Next/previous controls
+function changeSlide(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  const slides = document.getElementsByClassName("slide");
+  const dots = document.getElementsByClassName("dot");
+
+  if (n >= slides.length) { slideIndex = 0; }
+  if (n < 0) { slideIndex = slides.length - 1; }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slides[slideIndex].style.display = "block";
+  dots[slideIndex].className += " active";
+}
